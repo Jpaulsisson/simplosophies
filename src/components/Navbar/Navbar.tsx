2600b75fa5a7c8af2042f4415717176bf696dbc5
Paul Sisson <paulsissonsemail@gmail.com>
2023-11-08 17:07:10 -0600

    Begin adding mock posts

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react'
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
     <nav>
-      <h1 className={styles.logo}>Simpl<span className={styles.logoSpan}>osophies</span></h1>
+      <Link className={styles.logo} href={'/'}>
+        <h1>Simpl<span className={styles.logoSpan}>osophies</span></h1>
+      </Link>
     </nav>
   )
 }
