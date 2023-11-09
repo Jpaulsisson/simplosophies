ab0d9a594c5710e911fba1ea59b01ee992ed10c1
Paul Sisson <paulsissonsemail@gmail.com>
2023-11-09 07:23:34 -0600

    Remove some of mock data

--- src/app/mockPosts.js
+++ src/app/mockPosts.js
@@ -3,22 +3,16 @@ const mockPosts = [
     id: 1,
     image: '#',
     title: 'Drinking Poison',
-    body: `I want to talk about hate. Not the deep-seeded loathing a super-villain holds for his/her nemesis. A much more real, everyday form of hate. The kind of hate that I'm referring to is one people usually hold for their ex-lovers, an ex-bestie or even a parent that wasn't what they were supposed to be. Let's cast the net just a bit wider and include people that have legitmately wronged us, regardless or what that relationship has been in the past.
-
-    We all have these people. I don't feel like I hold HATRED for these people in my own life, but I'm starting to think I'm wrong about that. What IS hate, truly? Oxford says "intense or passionate dislike". In this hyperbolic society that we've created, I was expecting a much stronger definition. I thought of hate more as wanting someone to actually be dead and to celebrate their deadness on it's arrival, not just like I don't want to ever see their stupid face again.
-    
-    I'll get onward to the point, with an example. Years ago, a girl that I had no plans to marry, cheated on me with my best friend in the world. Obviously both of those relationships ceased to exist in my life after that. In the 15 years since, I've spoken to her 20 times or so, and to him maybe 2 times.
-    
-    10 years after this event, I sat down at a bar and was talking with old buds about "the good ole days" and someone asked "do you ever talk to so-and-so anymore?" and like an absolute child, I responded with "Of course not! Screw that guy! We haven't talked since he slept with my girlfriend". To my surprise, nobody else seemed to care that he had done that. I felt like an injustice had been done. Why didn't his name disgust them too? Then I learned that they had become somewhat close friends with the girl. I was at a loss at this point. 
-
-    After some time reflecting on the response the other humans gave me, I had a thought. That guy, and that girl, they aren't even thinking about me right now. They don't feel my anger toward them. They don't feel my rage still burning at the thought of them, ten years after the fact. If they think of me at all, I'm sure it's in passing or potentially even in a remorseful way. Either way, they are not affected in the slightest by something that was still affecting me because I was refusing to let go of that hate.
-    
-    Thinking about what they did, made my skin burn and my blood boil, but WHY? I was refusing to forgive them. Refusing to accept that they are people and as such they are going to do things wrong. They hurt me, but does that mean all the love they showed me before was false? All the laughs we shared were meaningless? Fake? A big joke on me? Somehow I doubt they had been planning it for years and setting me up to hurt me. They just messed up. For years I drank the poison of hate. Every time I heard the names of people that had wronged me, hurt me, mistreated me, I drank it down. None of them know that. It never changed a single thing about their lives. My hatred never hurt them back. It was, however, causing me to feel things I didn't like to feel. Destroying my happiness. Avoiding places they might show up so I wouldn't have to think about it.
-    
-    When I finally forgave them, I felt such a true peace inside. If I saw either of them today, I'd hug them and tell them I love them and that I hope life has turned out well for them, because that's how I truly feel. I won't carry that hatred any longer. And I'll continue to rid myself of even these "small hates" so that I can become even more at peace with my life.
+    body: `
 
     The point I hope to make with this simplosophy is this:
   `,
-  simplosophy: 'Forgiveness gives us back everything we feel like we lost to the people that hurt us, and more.'
+  simplosophy: `Hatred destroys it's holder, while forgiveness empowers it's giver.`
+  },
+  {
+    id: 2,
+    image: '#',
+    title: 'Control Your Perspective, Control Your Life',
+    body: ``
   }
 ]
