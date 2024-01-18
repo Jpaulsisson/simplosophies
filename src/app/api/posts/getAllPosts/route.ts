5e38951264a46e4fc6a95194dbc906475f92063f
Paul Sisson <paulsissonsemail@gmail.com>
2024-01-18 11:09:59 -0600

    Add posts to db and get posts from db

--- src/app/api/posts/getAllPosts/route.ts
+++ src/app/api/posts/getAllPosts/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   try {
-    const posts = await prisma.posts.findMany()
+    const posts = await prisma.posts.findMany({ take: 1 })
     if (posts && posts.length > 0) {
       return NextResponse.json({
         data: posts,
