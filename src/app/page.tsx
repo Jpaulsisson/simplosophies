073b22f678e051070fbd4ceadf0034a3e24efaec
Paul Sisson <paulsissonsemail@gmail.com>
2024-01-23 18:56:55 -0600

    CSS cleanup and add intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,9 @@ export default function Home() {
         <h2 className={styles.welcome}>Hey there, welcome to Simplosophies.</h2>
         <p className={styles.subwelcome}>...it&apos;s basically my own personal LiveJournal.</p>
       </section>
+      <section className={styles.introContainer}>
+        <p className={styles.introContent}>This blog is about my journey to become a successful software developer, my thoughts on the multiple lives I&apos;ve lived, and really just about any other thing I feel like writing down. I hope it brings you, at minimum, a laugh, some hope or peace, or maybe just the comfort that someone else is weirder than you. Enjoy.</p>
+      </section>
       <section className={styles.recentPostsContainer}>
         <h2 className={styles.recentHeader}>Recent Posts</h2>
         {recents ?
