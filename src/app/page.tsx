804ad7762558fee5461491c969649baa25970e62
Paul Sisson <paulsissonsemail@gmail.com>
2024-01-27 19:22:22 -0600

    Drop intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,10 +27,7 @@ export default function Home() {
     <main className={styles.container}>
       <section className={styles.welcomeContainer}>
         <h2 className={styles.welcome}>Hey there, welcome to Simplosophies.</h2>
-        <p className={styles.subwelcome}>...it&apos;s basically my own personal LiveJournal.</p>
-      </section>
-      <section className={styles.introContainer}>
-        <p className={styles.introContent}>This blog is about my journey to become a successful software developer, my thoughts on the multiple lives I&apos;ve lived, and really just about any other thing I feel like writing down. I hope it brings you, at minimum, a laugh, some hope or peace, or maybe just the comfort that someone else is weirder than you. Enjoy.</p>
+        <p className={styles.subwelcome}>...it&apos;s basically my personal LiveJournal.</p>
       </section>
       <section className={styles.recentPostsContainer}>
         <h2 className={styles.recentHeader}>Recent Posts</h2>
