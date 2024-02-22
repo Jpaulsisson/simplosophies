6210662b02de85518b3eecba83d6904a58072897
Paul Sisson <paulsissonsemail@gmail.com>
2024-02-22 15:46:32 -0600

    Add no holiday state

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -30,11 +30,16 @@ export default function Discover() {
       <div className={styles.pickerAndListContainer}>
         <Datepicker setHolidays={setHolidays} selectedCountry={selectedCountry} />
         <ul className={styles.holidayList}>
-          {
-            holidays &&
+          {holidays &&
             holidays.map((holiday) => {
               return <li className={styles.holidayItem} key={holiday.name}>{holiday.name}</li>
             })
+          }
+          {holidays.length === 0 && 
+          <div>
+            <h3>Search for holidays!</h3>
+            <p>If nothing appears here after clicking ok that means there are no holidays known for the chosen country and the given date</p>
+          </div>
           }
         </ul>
       </div>
